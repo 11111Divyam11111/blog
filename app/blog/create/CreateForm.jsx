83b3bc49af5ba2d0eef66a44b41ef5986c2bb524
11111Divyam11111 <126981502+11111Divyam11111@users.noreply.github.com>
2024-05-27 02:38:57 +0530

    undo previous change

--- app/blog/create/CreateForm.jsx
+++ app/blog/create/CreateForm.jsx
@@ -25,7 +25,7 @@ export default function CreateForm() {
     setUserId(user_ki_id);
 
     try {
-      const res = await fetch("https://blog-iota-ruddy.vercel.app/api/blog", {
+      const res = await fetch("http://localhost:3000/api/blog", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ title, blog }),
